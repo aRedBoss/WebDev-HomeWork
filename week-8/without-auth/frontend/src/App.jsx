af177da5740ee46998f2d1ef8b76d88e81147fcc
Omar <aldulaimiomar111@gmail.com>
2025-03-10 10:08:59 +0200

    without-auth DONE

--- week-8/without-auth/frontend/src/App.jsx
+++ week-8/without-auth/frontend/src/App.jsx
@@ -1,27 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
 // pages & components
+import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/HomePage";
 import AddPropertyPage from "./pages/AddPropertyPage";
-import Navbar from "./components/Navbar";
-import NotFoundPage from "./pages/NotFoundPage"
+import PropertyPage from "./pages/PropertyPage";
+import EditPropertyPage from "./pages/EditPropertyPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
-
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/add-property" element={<AddPropertyPage />} />
-            <Route path='*' element={<NotFoundPage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/add-property" element={<AddPropertyPage />} />
+        <Route path="/edit-property/:id" element={<EditPropertyPage />} />
+        <Route path="/properties/:id" element={<PropertyPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
   );
-}
+
+  return <RouterProvider router={router} />;
+};
 
 export default App;
